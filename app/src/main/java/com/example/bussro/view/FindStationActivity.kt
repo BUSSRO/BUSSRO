da70aa54dd77c5ed4384c1ff534a77be12eaa755
Juyeon <otu165@naver.com>
2021-08-26 00:39:05 +0900

    feat(UI): activity_find_station

---
+++ app/src/main/java/com/example/bussro/view/FindStationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bussro.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bussro.R
+
+class FindStationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_station)
+    }
+}
