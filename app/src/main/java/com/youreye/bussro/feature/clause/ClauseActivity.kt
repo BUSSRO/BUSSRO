bbbf456886301d42927dd645cfdc1dbe6aecce92
Juyeon <otu1650@gmail.com>
2022-01-18 00:59:47 +0900

    feat: ClauseActivity.kt

--- app/src/main/java/com/youreye/bussro/feature/clause/ClauseActivity.kt
+++ app/src/main/java/com/youreye/bussro/feature/clause/ClauseActivity.kt
@@ -1,8 +1,18 @@
 package com.youreye.bussro.feature.clause
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.view.View
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
@@ -22,22 +32,20 @@ class ClauseActivity : AppCompatActivity() {
     }
 
     private fun initVar() {
+        /* CheckBox listener */
+        binding.cbClauseFirst.setOnClickListener { onCheckChanged(binding.cbClauseFirst) }
+        binding.cbClauseSecond.setOnClickListener { onCheckChanged(binding.cbClauseSecond) }
+        binding.cbClauseThird.setOnClickListener { onCheckChanged(binding.cbClauseThird) }
+        binding.cbClauseSelectAll.setOnClickListener { onCheckChanged(binding.cbClauseSelectAll) }
+
+        /* 텍스트 색상 커스텀 */
+        changeTextColor(binding.cbClauseFirst, 0, 4, resources.getColor(R.color.yellow))
+        changeTextColor(binding.cbClauseSecond, 0, 4, resources.getColor(R.color.yellow))
+        changeTextColor(binding.cbClauseDesc2, 0, 11, resources.getColor(R.color.orange))
+
         /* 뒤로가기 */
         binding.ibClauseBack.setOnClickListener {
             finish()
-        }
-
-        /* 전체 동의하기 */
-        binding.cbClauseSelectAll.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked) {
-                binding.cbClauseFirst.isChecked = true
-                binding.cbClauseSecond.isChecked = true
-                binding.cbClauseThird.isChecked = true
-            } else {
-                binding.cbClauseFirst.isChecked = false
-                binding.cbClauseSecond.isChecked = false
-                binding.cbClauseThird.isChecked = false
-            }
         }
 
         /* 앱 시작하기 */
@@ -51,6 +59,47 @@ class ClauseActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "필수 약관에 동의해주세요.", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    /* 텍스트 색상 변경 */
+    private fun changeTextColor(cb: CheckBox, start: Int, end: Int, color: Int) {
+        val text = cb.text.toString()
+        val builder = SpannableStringBuilder(text)
+        val colorSpan = ForegroundColorSpan(color)
+        builder.setSpan(colorSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        cb.text = builder
+    }
+
+    /* 이용약관 동의 여부에 따라 뷰 상태 변화 */
+    private fun onCheckChanged(compoundButton: CompoundButton) {
+        // 전체 동의
+        when (compoundButton.id) {
+            R.id.cb_clause_select_all -> {
+                if (binding.cbClauseSelectAll.isChecked) {
+                    binding.cbClauseFirst.isChecked = true
+                    binding.cbClauseSecond.isChecked = true
+                    binding.cbClauseThird.isChecked = true
+                } else {
+                    binding.cbClauseFirst.isChecked = false
+                    binding.cbClauseSecond.isChecked = false
+                    binding.cbClauseThird.isChecked = false
+                }
+            }
+            else -> {
+                binding.cbClauseSelectAll.isChecked = (
+                        binding.cbClauseFirst.isChecked
+                        && binding.cbClauseSecond.isChecked
+                        && binding.cbClauseThird.isChecked
+                )
+            }
+        }
+
+        // 앱 시작하기
+        if (binding.cbClauseFirst.isChecked && binding.cbClauseSecond.isChecked) {
+            binding.txtClauseStart.setBackgroundColor(resources.getColor(R.color.yellow))
+        } else {
+            binding.txtClauseStart.setBackgroundColor(resources.getColor(R.color.light_gray))
         }
     }
 }
